d1048261eca54da3e2b011fe74d8677a78a72a7d
benscott <ben@benscott.co.uk>
2015-03-25 11:43:19 +0000

    Updated namespacing - #19

--- ckanext/__init__.py
+++ ckanext/__init__.py
@@ -1,18 +1,7 @@
-#!/usr/bin/env python
-# encoding: utf-8
-"""
-Created by 'bens3' on 2013-06-21.
-Copyright (c) 2013 'bens3'. All rights reserved.
-"""
-
-import sys
-import os
-
-
-def main():
-    pass
-
-
-if __name__ == '__main__':
-    main()
-
+# this is a namespace package
+try:
+    import pkg_resources
+    pkg_resources.declare_namespace(__name__)
+except ImportError:
+    import pkgutil
+    __path__ = pkgutil.extend_path(__path__, __name__)
