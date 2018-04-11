9320a4b38127e3f4567a5b08c1190cc687175076
Alice Butcher <alycejenni@gmail.com>
2018-04-11 15:07:36 +0100

    tidied setup.py

--- setup.py
+++ setup.py
@@ -4,34 +4,34 @@
 # This file is part of ckanext-doi
 # Created by the Natural History Museum in London, UK
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 version = u'0.2'
 
 setup(
-	name=u'ckanext-doi',
-	version=version,
-	description=u'CKAN extension for assigning a DOI to datasets',
-	classifiers=[],
-	keywords=u'',
-	author=u'Ben Scott',
-	author_email=u'ben@benscott.co.uk',
-	url=u'',
-	license=u'',
+    name=u'ckanext-doi',
+    version=version,
+    description=u'CKAN extension for assigning a DOI to datasets',
+    classifiers=[],
+    keywords=u'',
+    author=u'Ben Scott',
+    author_email=u'ben@benscott.co.uk',
+    url=u'',
+    license=u'',
     packages=find_packages(exclude=[u'tests']),
     namespace_packages=[u'ckanext', u'ckanext.doi'],
-	include_package_data=True,
-	zip_safe=False,
-	install_requires=[
-		u'requests',
-		u'xmltodict'
-	],
-	entry_points=\
-	u'''
-    [ckan.plugins]
-    	doi=ckanext.doi.plugin:DOIPlugin
-
-    [paste.paster_command]
-        doi=ckanext.doi.commands.doi:DOICommand
-	''',
-)
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        u'requests',
+        u'xmltodict'
+        ],
+    entry_points= \
+        u'''
+        [ckan.plugins]
+            doi=ckanext.doi.plugin:DOIPlugin
+    
+        [paste.paster_command]
+            doi=ckanext.doi.commands.doi:DOICommand
+        ''',
+    )
