0f68b46ff94cb2a132f1d860c66998c17f1de3ef
Ginger Butcher <alycejenni@gmail.com>
2020-11-11 18:11:55 +0000

    Test if the DOI was actually created on DataCite

--- ckanext/doi/tests/test_api.py
+++ ckanext/doi/tests/test_api.py
@@ -4,6 +4,8 @@
 # This file is part of ckanext-doi
 # Created by the Natural History Museum in London, UK
 
+import time
+
 import nose
 from ckanext.doi.lib.api import DataciteClient
 from ckantest.models import TestBase
@@ -25,12 +27,15 @@ class TestAPI(TestBase):
 
     def test_mint_new_doi(self):
         api = DataciteClient()
-        doi = constants.XML_DICT[u'identifier'][u'identifier']
+        doi = api.generate_doi()
         pkg_id = u'abcd1234'
         with nose.tools.assert_raises(DataCiteError):
             api.mint_doi(doi, pkg_id)
         api.set_metadata(doi, constants.XML_DICT)
         api.mint_doi(doi, pkg_id)
+        time.sleep(10)  # give datacite time to update
+        datacite_url = api.client.doi_get(doi)
+        nose.tools.assert_is_not_none(datacite_url)
 
     def test_datacite_authentication(self):
         api = DataciteClient()
