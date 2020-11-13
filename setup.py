bb4916ffb7c30fbfb5d9c20b0011e032f01a9f1a
Ginger Butcher <alycejenni@gmail.com>
2020-11-13 12:31:52 +0000

    Change jsonschema version
    Travis is being difficult

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     install_requires=[
         'requests',
         'xmltodict==0.9.0',
-        'jsonschema>=2.5.1',
+        'jsonschema==3.2.0',
         'datacite==1.0.1'
         ],
     entry_points= \
