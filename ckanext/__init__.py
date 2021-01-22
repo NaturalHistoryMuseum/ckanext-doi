4813fc6d04bd8bf1d605adf0d7168f8c167786ec
Josh Humphries <j.humphries@nhm.ac.uk>
2021-01-22 16:37:24 +0000

    Apparently we still need the namespace package declaration
    Not exactly sure why, but hey ho.

---
+++ ckanext/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# This file is part of ckanext-doi
+# Created by the Natural History Museum in London, UK
+
+try:
+    import pkg_resources
+    pkg_resources.declare_namespace(__name__)
+except ImportError:
+    import pkgutil
+    __path__ = pkgutil.extend_path(__path__, __name__)
