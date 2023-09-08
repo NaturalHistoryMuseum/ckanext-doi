0c83d9475b194f76e63bf8109353e3f0f7bfcedf
Josh Humphries <j.humphries@nhm.ac.uk>
2023-09-08 11:09:40 +0100

    fix: add new functions to support CKAN 2.10
    In CKAN 2.10, they have renamed the IPackageController hooks. This really should have been a major change as it breaks the public API but hey ho here we are. To accommodate this while also supporting CKAN 2.9, I have renamed the hook functions we were using to their new CKAN 2.10 variants and then added functions which just call those functions which have the old CKAN 2.9 names. This is ok in this extension as we're not using any interfaces which have a name clash with the old names.
    
    The old names should be removed as soon as we stop supporting CKAN 2.9 cause it's ugly stuff.

--- ckanext/doi/plugin.py
+++ ckanext/doi/plugin.py
@@ -37,20 +37,20 @@ class DOIPlugin(SingletonPlugin, toolkit.DefaultDatasetForm):
         """
         Adds templates.
         """
-        toolkit.add_template_directory(config, 'theme/templates')
+        toolkit.add_template_directory(config, "theme/templates")
 
     ## IPackageController
-    def after_create(self, context, pkg_dict):
+    def after_dataset_create(self, context, pkg_dict):
         """
         A new dataset has been created, so we need to create a new DOI.
 
         NB: This is called after creation of a dataset, before resources have been
         added, so state = draft.
         """
-        DOIQuery.read_package(pkg_dict['id'], create_if_none=True)
+        DOIQuery.read_package(pkg_dict["id"], create_if_none=True)
 
     ## IPackageController
-    def after_update(self, context, pkg_dict):
+    def after_dataset_update(self, context, pkg_dict):
         """
         Dataset has been created/updated.
 
@@ -58,17 +58,17 @@ class DOIPlugin(SingletonPlugin, toolkit.DefaultDatasetForm):
         network.
         """
         # Is this active and public? If so we need to make sure we have an active DOI
-        if pkg_dict.get('state', 'active') == 'active' and not pkg_dict.get(
-            'private', False
+        if pkg_dict.get("state", "active") == "active" and not pkg_dict.get(
+            "private", False
         ):
-            package_id = pkg_dict['id']
+            package_id = pkg_dict["id"]
 
             # remove user-defined update schemas first (if needed)
-            context.pop('schema', None)
+            context.pop("schema", None)
 
             # Load the package_show version of the dict
-            pkg_show_dict = toolkit.get_action('package_show')(
-                context, {'id': package_id}
+            pkg_show_dict = toolkit.get_action("package_show")(
+                context, {"id": package_id}
             )
 
             # Load or create the local DOI (package may not have a DOI if extension was loaded
@@ -84,35 +84,53 @@ class DOIPlugin(SingletonPlugin, toolkit.DefaultDatasetForm):
                 # metadata gets created before minting
                 client.set_metadata(doi.identifier, xml_dict)
                 client.mint_doi(doi.identifier, package_id)
-                toolkit.h.flash_success('DataCite DOI created')
+                toolkit.h.flash_success("DataCite DOI created")
             else:
                 same = client.check_for_update(doi.identifier, xml_dict)
                 if not same:
                     # Not the same, so we want to update the metadata
                     client.set_metadata(doi.identifier, xml_dict)
-                    toolkit.h.flash_success('DataCite DOI metadata updated')
+                    toolkit.h.flash_success("DataCite DOI metadata updated")
 
         return pkg_dict
 
     # IPackageController
-    def after_show(self, context, pkg_dict):
+    def after_dataset_show(self, context, pkg_dict):
         """
         Add the DOI details to the pkg_dict so it can be displayed.
         """
-        doi = DOIQuery.read_package(pkg_dict['id'])
+        doi = DOIQuery.read_package(pkg_dict["id"])
         if doi:
-            pkg_dict['doi'] = doi.identifier
-            pkg_dict['doi_status'] = True if doi.published else False
-            pkg_dict['domain'] = get_site_url().replace('http://', '')
-            pkg_dict['doi_date_published'] = (
-                datetime.strftime(doi.published, '%Y-%m-%d') if doi.published else None
+            pkg_dict["doi"] = doi.identifier
+            pkg_dict["doi_status"] = True if doi.published else False
+            pkg_dict["domain"] = get_site_url().replace("http://", "")
+            pkg_dict["doi_date_published"] = (
+                datetime.strftime(doi.published, "%Y-%m-%d") if doi.published else None
             )
-            pkg_dict['doi_publisher'] = toolkit.config.get('ckanext.doi.publisher')
+            pkg_dict["doi_publisher"] = toolkit.config.get("ckanext.doi.publisher")
+
+    def after_create(self, *args, **kwargs):
+        """
+        CKAN 2.9 compat version of after_dataset_create.
+        """
+        return self.after_dataset_create(*args, **kwargs)
+
+    def after_update(self, *args, **kwargs):
+        """
+        CKAN 2.9 compat version of after_dataset_update.
+        """
+        return self.after_dataset_update(*args, **kwargs)
+
+    def after_show(self, *args, **kwargs):
+        """
+        CKAN 2.9 compat version of after_dataset_show.
+        """
+        return self.after_dataset_show(*args, **kwargs)
 
     # ITemplateHelpers
     def get_helpers(self):
         return {
-            'package_get_year': package_get_year,
-            'now': datetime.now,
-            'get_site_title': get_site_title,
+            "package_get_year": package_get_year,
+            "now": datetime.now,
+            "get_site_title": get_site_title,
         }
