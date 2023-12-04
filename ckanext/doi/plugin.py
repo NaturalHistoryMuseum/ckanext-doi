a758ceca1fc8d18fd5e2c8edb377d9e9cf383076
Ginger Butcher <alycejenni@gmail.com>
2023-12-04 13:24:07 +0000

    style: use single quotes

--- ckanext/doi/plugin.py
+++ ckanext/doi/plugin.py
@@ -42,7 +42,7 @@ class DOIPlugin(SingletonPlugin, toolkit.DefaultDatasetForm):
         """
         Adds templates.
         """
-        toolkit.add_template_directory(config, "theme/templates")
+        toolkit.add_template_directory(config, 'theme/templates')
 
     ## IPackageController
     def after_dataset_create(self, context, pkg_dict):
@@ -52,7 +52,7 @@ class DOIPlugin(SingletonPlugin, toolkit.DefaultDatasetForm):
         NB: This is called after creation of a dataset, before resources have been
         added, so state = draft.
         """
-        DOIQuery.read_package(pkg_dict["id"], create_if_none=True)
+        DOIQuery.read_package(pkg_dict['id'], create_if_none=True)
 
     ## IPackageController
     def after_dataset_update(self, context, pkg_dict):
@@ -63,17 +63,17 @@ class DOIPlugin(SingletonPlugin, toolkit.DefaultDatasetForm):
         network.
         """
         # Is this active and public? If so we need to make sure we have an active DOI
-        if pkg_dict.get("state", "active") == "active" and not pkg_dict.get(
-            "private", False
+        if pkg_dict.get('state', 'active') == 'active' and not pkg_dict.get(
+            'private', False
         ):
-            package_id = pkg_dict["id"]
+            package_id = pkg_dict['id']
 
             # remove user-defined update schemas first (if needed)
-            context.pop("schema", None)
+            context.pop('schema', None)
 
             # Load the package_show version of the dict
-            pkg_show_dict = toolkit.get_action("package_show")(
-                context, {"id": package_id}
+            pkg_show_dict = toolkit.get_action('package_show')(
+                context, {'id': package_id}
             )
 
             # Load or create the local DOI (package may not have a DOI if extension was loaded
@@ -89,13 +89,13 @@ class DOIPlugin(SingletonPlugin, toolkit.DefaultDatasetForm):
                 # metadata gets created before minting
                 client.set_metadata(doi.identifier, xml_dict)
                 client.mint_doi(doi.identifier, package_id)
-                toolkit.h.flash_success("DataCite DOI created")
+                toolkit.h.flash_success('DataCite DOI created')
             else:
                 same = client.check_for_update(doi.identifier, xml_dict)
                 if not same:
                     # Not the same, so we want to update the metadata
                     client.set_metadata(doi.identifier, xml_dict)
-                    toolkit.h.flash_success("DataCite DOI metadata updated")
+                    toolkit.h.flash_success('DataCite DOI metadata updated')
 
         return pkg_dict
 
@@ -104,15 +104,15 @@ class DOIPlugin(SingletonPlugin, toolkit.DefaultDatasetForm):
         """
         Add the DOI details to the pkg_dict so it can be displayed.
         """
-        doi = DOIQuery.read_package(pkg_dict["id"])
+        doi = DOIQuery.read_package(pkg_dict['id'])
         if doi:
-            pkg_dict["doi"] = doi.identifier
-            pkg_dict["doi_status"] = True if doi.published else False
-            pkg_dict["domain"] = get_site_url().replace("http://", "")
-            pkg_dict["doi_date_published"] = (
-                datetime.strftime(doi.published, "%Y-%m-%d") if doi.published else None
+            pkg_dict['doi'] = doi.identifier
+            pkg_dict['doi_status'] = True if doi.published else False
+            pkg_dict['domain'] = get_site_url().replace('http://', '')
+            pkg_dict['doi_date_published'] = (
+                datetime.strftime(doi.published, '%Y-%m-%d') if doi.published else None
             )
-            pkg_dict["doi_publisher"] = toolkit.config.get("ckanext.doi.publisher")
+            pkg_dict['doi_publisher'] = toolkit.config.get('ckanext.doi.publisher')
 
     def after_create(self, *args, **kwargs):
         """
@@ -135,8 +135,8 @@ class DOIPlugin(SingletonPlugin, toolkit.DefaultDatasetForm):
     # ITemplateHelpers
     def get_helpers(self):
         return {
-            "package_get_year": package_get_year,
-            "now": datetime.now,
-            "get_site_title": get_site_title,
-            "doi_test_mode": doi_test_mode,
+            'package_get_year': package_get_year,
+            'now': datetime.now,
+            'get_site_title': get_site_title,
+            'doi_test_mode': doi_test_mode,
         }
